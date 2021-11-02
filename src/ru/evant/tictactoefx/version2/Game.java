6d0fe79deab49d6a8aa2ddee382ff2c56a265ba7
Anton Evseev <58934670+AntonEvseev74@users.noreply.github.com>
2021-11-02 14:16:06 +0500

    Исправлена проверка состояния игры И выявления победителя

--- src/ru/evant/tictactoefx/version2/Game.java
+++ src/ru/evant/tictactoefx/version2/Game.java
@@ -22,14 +22,16 @@ public class Game extends Pane {
     static int emptyCellCount = 9;
     public static boolean gameOver = false;
 
-    private final String textWin = "Конец игры! Вы выиграли!";
-    private final String textLos = "Конец игры! Вы проиграли!";
+    private final String wins = "Конец игры! Вы выиграли!";
+    private final String loss = "Конец игры! Вы проиграли!";
+    private final String draw = "Конец игры! Ничья!";
+    private String gameState = "";
+
     public final String X = " X ";   // Крестик
     public final String O = " O ";   // Нолик
 
     private final String[] gameField = new String[9]; // Создать массив длинной 9. Это 9 ячеек для поля 3 на 3 ячейки.
     private final boolean[] isEmpty = new boolean[9];
-
     {
         Arrays.fill(isEmpty, true);
         Arrays.fill(gameField, "");
@@ -39,7 +41,6 @@ public class Game extends Pane {
 
 
     private Image[] imagesX;
-
     {
         try {
             imagesX = new Image[5];
@@ -52,7 +53,6 @@ public class Game extends Pane {
     }
 
     private Image[] imagesO;
-
     {
         try {
             imagesO = new Image[5];
@@ -67,7 +67,6 @@ public class Game extends Pane {
     int countImgX = 4;
     int countImgO = 4;
     private final ImageView[] imgX = new ImageView[5];//(imageX);
-
     {
         for (int i = 0; i < imgX.length; i++) {
             imgX[i] = new ImageView(imagesX[i]);
@@ -77,7 +76,6 @@ public class Game extends Pane {
     }
 
     private final ImageView[] imgO = new ImageView[5];//(imageO);
-
     {
         for (int i = 0; i < imgO.length; i++) {
             imgO[i] = new ImageView(imagesO[i]);
@@ -86,17 +84,20 @@ public class Game extends Pane {
         }
     }
 
+
     public Game() {
         start();
     }
 
+    /* Начать игру. Рисует игровое поле и ходы игроков. */
     public void start() {
         newRoot();
         createButtons();
         gameMove();
         setButtons();
-    }
+    } // Конец метода start()
 
+    /* Создать кнопки */
     public void createButtons() {
         for (int i = 0; i < 9; i++) {
             Button btn = new Button();
@@ -104,9 +105,39 @@ public class Game extends Pane {
             btn.setPrefHeight(100);
             buttons.add(btn);
         }
-    }
+    } // Конец метода createButtons()
 
+    // Обработка ходов игроков */
     public void gameMove() {
+        for (int i = 0; i < buttons.size(); i++) {
+            // Слушатель нажатия кнопки
+            int index = i;
+            buttons.get(index).setOnAction(event -> {
+                if (!gameOver) {
+                    // Ход пользователя
+                    moveUser(index);
+
+                    //ход пк
+                    moveAI();
+                }
+            });
+        }
+    } // Конец метода gameMove()
+
+    /* Создать контейнер для игрового поля из 9ти кнопок */
+    public void newRoot() {
+        root = new GridPane();
+        root.setPadding(new Insets(10, 10, 10, 10)); // отступы
+        root.setHgap(5); // горизонтальные отступы между строками
+        root.setVgap(5); // вертикальные отступы между столбцами
+    } // Конец метода newRoot()
+
+    /* Установить кнопки в контейнер игрового поля */
+    public void setButtons() {
+        for (Button b : buttons) {
+            root.getChildren().add(b); // добавить кнопки поле в таблицу
+        }
+
         for (int i = 0; i < buttons.size(); i++) {
             int column = 0;
             int row = 0;
@@ -121,35 +152,9 @@ public class Game extends Pane {
                 column = i - 6;
                 row = 2;
             }
-            GridPane.setConstraints(buttons.get(i), column, row); // установить позицию текстового поля в таблице
-
-            // Слушатель нажатия кнопки
-            int index = i;
-            buttons.get(index).setOnAction(event -> {
-
-                if (!gameOver) {
-                    // Ход пользователя
-                    moveUser(index);
-
-                    //ход пк
-                    moveAI();
-                }
-            });
+            GridPane.setConstraints(buttons.get(i), column, row); // установить позицию кнопки в контейнере(таблице)
         }
-    }
-
-    public void setButtons() {
-        for (Button b : buttons) {
-            root.getChildren().add(b); // добавить кнопки поле в таблицу
-        }
-    }
-
-    public void newRoot() {
-        root = new GridPane();
-        root.setPadding(new Insets(10, 10, 10, 10)); // отступы
-        root.setHgap(5); // горизонтальные отступы между строками
-        root.setVgap(5); // вертикальные отступы между столбцами
-    }
+    } // Конец метода setButtons()
 
     /* Ход игрока */
     public void moveUser(int index) {
@@ -163,8 +168,8 @@ public class Game extends Pane {
                 isEmpty[index] = false;
             }
         } else {
-            System.out.println(textLos);
-            showAlert(textLos);
+            System.out.println(gameState);//(textLoss);
+            showAlert(gameState);//(textLoss);
         }
     } // Конец метода moveUser()
 
@@ -183,53 +188,57 @@ public class Game extends Pane {
                 checkGameOver();
 
                 if (gameOver) {
-                    System.out.println(textLos);
-                    showAlert(textLos);
+                    System.out.println(gameState);
+                    showAlert(gameState);
                 }
             } else moveAI(); // Иначе сново запустить ход компьютера
         } else {
-            System.out.println(textWin);
-            showAlert(textWin);
+            System.out.println(gameState);//(textWins);
+            showAlert(gameState);//(textWins);
         }
     } // конец метода moveAI()
 
     /* Проверка выигрышных комбинаций, три в ряд */
     private void checkGameOver() {
-        if (emptyCellCount == 0) gameOver = true;
+        if (emptyCellCount == 0) gameOver(draw);
 
-        if (gameField[0].equals(X) && gameField[1].equals(X) && gameField[2].equals(X)) gameOver = true;
-        if (gameField[0].equals(O) && gameField[1].equals(O) && gameField[2].equals(O)) gameOver = true;
+        if (gameField[0].equals(X) && gameField[1].equals(X) && gameField[2].equals(X)) gameOver(wins);
+        if (gameField[0].equals(O) && gameField[1].equals(O) && gameField[2].equals(O)) gameOver(loss);
 
-        if (gameField[3].equals(X) && gameField[4].equals(X) && gameField[5].equals(X)) gameOver = true;
-        if (gameField[3].equals(O) && gameField[4].equals(O) && gameField[5].equals(O)) gameOver = true;
+        if (gameField[3].equals(X) && gameField[4].equals(X) && gameField[5].equals(X)) gameOver(wins);
+        if (gameField[3].equals(O) && gameField[4].equals(O) && gameField[5].equals(O)) gameOver(loss);
 
-        if (gameField[6].equals(X) && gameField[7].equals(X) && gameField[8].equals(X)) gameOver = true;
-        if (gameField[6].equals(O) && gameField[7].equals(O) && gameField[8].equals(O)) gameOver = true;
+        if (gameField[6].equals(X) && gameField[7].equals(X) && gameField[8].equals(X)) gameOver(wins);
+        if (gameField[6].equals(O) && gameField[7].equals(O) && gameField[8].equals(O)) gameOver(loss);
 
-        if (gameField[0].equals(X) && gameField[3].equals(X) && gameField[6].equals(X)) gameOver = true;
-        if (gameField[0].equals(O) && gameField[3].equals(O) && gameField[6].equals(O)) gameOver = true;
+        if (gameField[0].equals(X) && gameField[3].equals(X) && gameField[6].equals(X)) gameOver(wins);
+        if (gameField[0].equals(O) && gameField[3].equals(O) && gameField[6].equals(O)) gameOver(loss);
 
-        if (gameField[1].equals(X) && gameField[4].equals(X) && gameField[7].equals(X)) gameOver = true;
-        if (gameField[1].equals(O) && gameField[4].equals(O) && gameField[7].equals(O)) gameOver = true;
+        if (gameField[1].equals(X) && gameField[4].equals(X) && gameField[7].equals(X)) gameOver(wins);
+        if (gameField[1].equals(O) && gameField[4].equals(O) && gameField[7].equals(O)) gameOver(loss);
 
-        if (gameField[2].equals(X) && gameField[5].equals(X) && gameField[8].equals(X)) gameOver = true;
-        if (gameField[2].equals(O) && gameField[5].equals(O) && gameField[8].equals(O)) gameOver = true;
+        if (gameField[2].equals(X) && gameField[5].equals(X) && gameField[8].equals(X)) gameOver(wins);
+        if (gameField[2].equals(O) && gameField[5].equals(O) && gameField[8].equals(O)) gameOver(loss);
 
-        if (gameField[0].equals(X) && gameField[4].equals(X) && gameField[8].equals(X)) gameOver = true;
-        if (gameField[0].equals(O) && gameField[4].equals(O) && gameField[8].equals(O)) gameOver = true;
+        if (gameField[0].equals(X) && gameField[4].equals(X) && gameField[8].equals(X)) gameOver(wins);
+        if (gameField[0].equals(O) && gameField[4].equals(O) && gameField[8].equals(O)) gameOver(loss);
 
-        if (gameField[2].equals(X) && gameField[4].equals(X) && gameField[6].equals(X)) gameOver = true;
-        if (gameField[2].equals(O) && gameField[4].equals(O) && gameField[6].equals(O)) gameOver = true;
-
-
+        if (gameField[2].equals(X) && gameField[4].equals(X) && gameField[6].equals(X)) gameOver(wins);
+        if (gameField[2].equals(O) && gameField[4].equals(O) && gameField[6].equals(O)) gameOver(loss);
     } // конец метода checkGameOver()
+
+    /* Установить конец игры и текст */
+    private void gameOver(String text) {
+        gameOver = true;
+        gameState = text;
+    }
 
     /* Возвращает контейнер типа GridPane */
     public GridPane getRoot() {
         return root;
     } // Конец метода getRoot()
 
-
+    /* Создать окно с сообщением о победе/проигрыше и предложении начать новую игру */
     private void showAlert(String text) {
 
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
@@ -239,9 +248,9 @@ public class Game extends Pane {
 
         Optional<ButtonType> option = alert.showAndWait();
         if (option.get() == ButtonType.OK) startNewGame(); // Начать игру сначала
-        if (option.get() == ButtonType.CANCEL) alert.close(); // Закрыть алерт
+        if (option.get() == ButtonType.CANCEL) closeGame(alert); // Закрыть программу
 
-    }
+    } // Конец метода showAlert()
 
     /* Начать новую игру */
     private void startNewGame() {
@@ -250,6 +259,12 @@ public class Game extends Pane {
         gameOver = false;
         emptyCellCount = gameField.length;
         Main.startGame();
+    } // Конец метода startNewGame()
+
+    /* Закрыть игру */
+    private void closeGame(Alert alert){
+        alert.close(); // Закрыть алерт
+        Main.primaryStage.close(); // Закрыть игру
     }
 
 }
