10f25aed6265de5ce2a986f4c5ec5d4861586da9
Anton Evseev <58934670+AntonEvseev74@users.noreply.github.com>
2021-11-02 14:39:44 +0500

    исправлено: нельзя выбрать занятое поле

---
+++ src/ru/evant/tictactoefx/version3/Game.java
@@ -0,0 +1,269 @@
+package ru.evant.tictactoefx.version3;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+
+public class Game extends Pane {
+
+    private GridPane root;
+
+    static int emptyCellCount = 9;
+    public static boolean gameOver = false;
+
+    private final String wins = "Конец игры! Вы выиграли!";
+    private final String loss = "Конец игры! Вы проиграли!";
+    private final String draw = "Конец игры! Ничья!";
+    private String gameState = "";
+
+    public final String X = " X ";   // Крестик
+    public final String O = " O ";   // Нолик
+
+    private final String[] gameField = new String[9]; // Создать массив длинной 9. Это 9 ячеек для поля 3 на 3 ячейки.
+    private final boolean[] isEmpty = new boolean[9];
+    {
+        Arrays.fill(isEmpty, true);
+        Arrays.fill(gameField, "");
+    }
+
+    private final ArrayList<Button> buttons = new ArrayList<>();
+
+
+    private Image[] imagesX;
+    {
+        try {
+            imagesX = new Image[5];
+            for (int i = 0; i < imagesX.length; i++) {
+                imagesX[i] = new Image(new FileInputStream("assets/x.png"));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Image[] imagesO;
+    {
+        try {
+            imagesO = new Image[5];
+            for (int i = 0; i < imagesO.length; i++) {
+                imagesO[i] = new Image(new FileInputStream("assets/o.png"));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    int countImgX = 4;
+    int countImgO = 4;
+    private final ImageView[] imgX = new ImageView[5];//(imageX);
+    {
+        for (int i = 0; i < imgX.length; i++) {
+            imgX[i] = new ImageView(imagesX[i]);
+            imgX[i].setFitWidth(80);
+            imgX[i].setFitHeight(80);
+        }
+    }
+
+    private final ImageView[] imgO = new ImageView[5];//(imageO);
+    {
+        for (int i = 0; i < imgO.length; i++) {
+            imgO[i] = new ImageView(imagesO[i]);
+            imgO[i].setFitWidth(80);
+            imgO[i].setFitHeight(80);
+        }
+    }
+
+
+    public Game() {
+        start();
+    }
+
+    /* Возвращает контейнер типа GridPane */
+    public GridPane getRoot() {
+        return root;
+    } // Конец метода getRoot()
+
+    /* Начать игру. Рисует игровое поле и ходы игроков. */
+    private void start() {
+        newRoot();
+        createButtons();
+        gameMove();
+        setButtons();
+    } // Конец метода start()
+
+    /* Создать кнопки */
+    private void createButtons() {
+        for (int i = 0; i < 9; i++) {
+            Button btn = new Button();
+            btn.setPrefWidth(100);
+            btn.setPrefHeight(100);
+            buttons.add(btn);
+        }
+    } // Конец метода createButtons()
+
+    // Обработка ходов игроков */
+    private void gameMove() {
+        for (int i = 0; i < buttons.size(); i++) {
+            // Слушатель нажатия кнопки
+            int index = i;
+            buttons.get(index).setOnAction(event -> {
+                if (!gameOver) {
+                    // Ход пользователя
+                    moveUser(index);
+
+                    //ход пк
+                    moveAI();
+                }
+            });
+        }
+    } // Конец метода gameMove()
+
+    /* Создать контейнер для игрового поля из 9ти кнопок */
+    private void newRoot() {
+        root = new GridPane();
+        root.setPadding(new Insets(10, 10, 10, 10)); // отступы
+        root.setHgap(5); // горизонтальные отступы между строками
+        root.setVgap(5); // вертикальные отступы между столбцами
+    } // Конец метода newRoot()
+
+    /* Установить кнопки в контейнер игрового поля */
+    private void setButtons() {
+        for (Button b : buttons) {
+            root.getChildren().add(b); // добавить кнопки поле в таблицу
+        }
+
+        for (int i = 0; i < buttons.size(); i++) {
+            int column = 0;
+            int row = 0;
+            if (i <= 2) {
+                column = i;
+            }
+            if (i >= 3 && i <= 5) {
+                column = i - 3;
+                row = 1;
+            }
+            if (i >= 6 && i <= 8) {
+                column = i - 6;
+                row = 2;
+            }
+            GridPane.setConstraints(buttons.get(i), column, row); // установить позицию кнопки в контейнере(таблице)
+        }
+    } // Конец метода setButtons()
+
+    /* Ход игрока */
+    private void moveUser(int index) {
+        checkGameOver();
+        if (!gameOver) {
+            if (isEmpty[index]) {
+                buttons.get(index).setGraphic(imgX[countImgX]);
+                buttons.get(index).setDisable(true);
+                gameField[index] = X;
+                countImgX--;
+                emptyCellCount--;
+                isEmpty[index] = false;
+            }
+        } else {
+            showAlert(gameState);
+        }
+    } // Конец метода moveUser()
+
+    /* Ход компьютера */
+    private void moveAI() {
+        checkGameOver();
+        if (!gameOver) {
+            int randomNumber = (int) (Math.random() * 8); // Присвоить пременной случайное число от 0 до 8. Это индексы ячеек игрового поля.
+
+            if (isEmpty[randomNumber]) {
+                buttons.get(randomNumber).setGraphic(imgO[countImgO]);
+                buttons.get(randomNumber).setDisable(true);
+                gameField[randomNumber] = O;
+                countImgO--;
+                emptyCellCount--;
+                isEmpty[randomNumber] = false;
+                checkGameOver();
+
+                if (gameOver) {
+                    showAlert(gameState);
+                }
+            } else moveAI(); // Иначе сново запустить ход компьютера
+        } else {
+            showAlert(gameState);
+        }
+    } // конец метода moveAI()
+
+    /* Проверка выигрышных комбинаций, три в ряд */
+    private void checkGameOver() {
+        if (emptyCellCount == 0) gameOver(draw);
+
+        if (gameField[0].equals(X) && gameField[1].equals(X) && gameField[2].equals(X)) gameOver(wins);
+        if (gameField[0].equals(O) && gameField[1].equals(O) && gameField[2].equals(O)) gameOver(loss);
+
+        if (gameField[3].equals(X) && gameField[4].equals(X) && gameField[5].equals(X)) gameOver(wins);
+        if (gameField[3].equals(O) && gameField[4].equals(O) && gameField[5].equals(O)) gameOver(loss);
+
+        if (gameField[6].equals(X) && gameField[7].equals(X) && gameField[8].equals(X)) gameOver(wins);
+        if (gameField[6].equals(O) && gameField[7].equals(O) && gameField[8].equals(O)) gameOver(loss);
+
+        if (gameField[0].equals(X) && gameField[3].equals(X) && gameField[6].equals(X)) gameOver(wins);
+        if (gameField[0].equals(O) && gameField[3].equals(O) && gameField[6].equals(O)) gameOver(loss);
+
+        if (gameField[1].equals(X) && gameField[4].equals(X) && gameField[7].equals(X)) gameOver(wins);
+        if (gameField[1].equals(O) && gameField[4].equals(O) && gameField[7].equals(O)) gameOver(loss);
+
+        if (gameField[2].equals(X) && gameField[5].equals(X) && gameField[8].equals(X)) gameOver(wins);
+        if (gameField[2].equals(O) && gameField[5].equals(O) && gameField[8].equals(O)) gameOver(loss);
+
+        if (gameField[0].equals(X) && gameField[4].equals(X) && gameField[8].equals(X)) gameOver(wins);
+        if (gameField[0].equals(O) && gameField[4].equals(O) && gameField[8].equals(O)) gameOver(loss);
+
+        if (gameField[2].equals(X) && gameField[4].equals(X) && gameField[6].equals(X)) gameOver(wins);
+        if (gameField[2].equals(O) && gameField[4].equals(O) && gameField[6].equals(O)) gameOver(loss);
+    } // конец метода checkGameOver()
+
+    /* Установить конец игры и текст */
+    private void gameOver(String text) {
+        gameOver = true;
+        gameState = text;
+    }
+
+    /* Создать окно с сообщением о победе/проигрыше и предложении начать новую игру */
+    private void showAlert(String text) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Игра закончена");
+        alert.setHeaderText(text);
+        alert.setContentText("Начать сначала?");
+
+        Optional<ButtonType> option = alert.showAndWait();
+        if (option.get() == ButtonType.OK) startNewGame(); // Начать игру сначала
+        if (option.get() == ButtonType.CANCEL) closeGame(alert); // Закрыть программу
+
+    } // Конец метода showAlert()
+
+    /* Начать новую игру */
+    private void startNewGame() {
+        Arrays.fill(isEmpty, true);
+        Arrays.fill(gameField, "");
+        gameOver = false;
+        emptyCellCount = gameField.length;
+        Main.startGame();
+    } // Конец метода startNewGame()
+
+    /* Закрыть игру */
+    private void closeGame(Alert alert){
+        alert.close(); // Закрыть алерт
+        Main.primaryStage.close(); // Закрыть игру
+    }
+
+}
---
+++ src/ru/evant/tictactoefx/version3/Main.java
@@ -0,0 +1,36 @@
+package ru.evant.tictactoefx.version3;
+
+// Игра: крестики нолики
+
+// Крестики-нолики — логическая игра между двумя противниками на квадратном поле 3 на 3 клетки.
+// Пользователь играет играет «крестиками», AI — «ноликами».
+
+// Правила:
+// Игроки по очереди ставят на свободные клетки поля 3×3 знаки (один всегда крестики, другой всегда нолики).
+// Первый, выстроивший в ряд 3 своих фигуры по вертикали, горизонтали или диагонали, выигрывает.
+// Первый ход делает игрок, ставящий крестики.
+// Обычно по завершении партии выигравшая сторона зачёркивает чертой свои три знака (нолика или крестика),
+// составляющих сплошной ряд. !!!!!!!!!!!!!!!!(не реализовано)!!!!!!!!!!!!!
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+static Stage primaryStage;
+    @Override
+    public void start(Stage primaryStage) {
+       Main.primaryStage = primaryStage;
+        startGame();
+    }
+
+    public static void startGame(){
+        primaryStage.setTitle("КрестикиНолики");
+        primaryStage.setScene(new Scene(new Game().getRoot(), 330, 330));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
